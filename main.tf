ad3ca721041e38b8e86add6a8daff27e669cc264
Shantanu3009 <91266681+Shantanu3009@users.noreply.github.com>
2025-01-28 12:09:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 resource "google_storage_bucket" "my_bucket" {
-  name     = "my-bucket-name"
-  location = "US"
+  name          = "jenkins-pipeline-bucket"
+  location      = "US"
   storage_class = "STANDARD"
+  project       = "shan-practice-dev"
 }
+
